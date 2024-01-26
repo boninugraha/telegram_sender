88146ae63bb8b05c895fd1718868ee879cb3f97f
nugrahacode <boni.nugraha@accenture.com>
2024-01-26 14:44:16 +0700

    Change path into variables

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 bot = telegram.Bot(token=TOKEN)
 
+document_path = ''
+image_path = ''
+
 async def send_message():
     await(asyncio.sleep(1))
     await bot.send_message(chat_id=CHAT_ID, text='Hello, World!')
 
 async def send_image():
     await(asyncio.sleep(2))
-    await bot.send_photo(chat_id=CHAT_ID, photo=open('/Users/boni.nugraha/Downloads/airflow.png', 'rb'))
+    await bot.send_photo(chat_id=CHAT_ID, photo=open(image_path, 'rb'))
 
 async def send_doc():
     await(asyncio.sleep(3))
-    await bot.send_document(chat_id=CHAT_ID, document=open('/Users/boni.nugraha/Downloads/test.xlsx', 'rb'))
+    await bot.send_document(chat_id=CHAT_ID, document=open(document_path, 'rb'))
 
 async def main():
     messages_queue = [send_message(), send_doc(), send_image()]
